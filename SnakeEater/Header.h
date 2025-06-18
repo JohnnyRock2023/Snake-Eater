3c918181acf82dc0b883a0be8f4e55651d3c8b63
JohnnyRock2023 <kutsenkoroma147@gmail.com>
2025-06-18 12:51:50 +0300

    Added Snakes

--- SnakeEater/Header.h
+++ SnakeEater/Header.h
@@ -9,7 +9,9 @@
 #include <thread>
 #include <vector>
 #include <ctime>
+#include <string>
 #include <random>
+#include <cmath>
 using namespace sf;
 using namespace std;
 
@@ -24,6 +26,10 @@ using namespace std;
 #define MAX_NUM_OF_OBJECTS 500
 #define STEP 5
 #define SPEED 12   // < 10 - Faster; > 10 - Slower
+#define MIN_SNAKE_SIZE 3
+#define MAX_SNAKE_SIZE 7
+#define SNAKE_SPEED 500
+#define SNAKE_MOVES 2
 
 extern int game_status;
 
@@ -46,15 +52,52 @@ public:
 	}
 };
 
+class Snake {
+private:
+	short int direction = 0;
+	short int size;
+	vector<Vector2f> body;
+
+public:
+	Snake(int size, int direction, vector<Vector2f> &body)
+	{
+		this->size = size;
+		this->body = body;
+		this->direction = direction;
+	};
+	int getSize() {
+		return size;
+	}
+	int getDirect() {
+		return direction;
+	}
+	void setDirect(int direction) {
+		this->direction = direction;
+	}
+	vector<Vector2f>& getBody() {
+		return body;
+	}
+	void setBody(vector<Vector2f> body) {
+		this->body = body;
+	}
+};
+
 extern vector<Object> objects;
+extern vector<Snake> snakes;
 extern RectangleShape GrassShape;
 extern RectangleShape RockShape;
 extern RectangleShape StumpShape;
 extern RectangleShape BushShape;
 extern RectangleShape Player;
+extern RectangleShape SnakeBodyShape;
+extern RectangleShape SnakeHeadShape;
 
 //FUNCTIONS
 
 //Kutsenko Roman
 void setObjectsPos();
 void fillTheMapWithObj(RenderWindow* window);
+bool isPosNew(Vector2f pos);
+void spawnSnakes(int count);
+void moveSnakes();
+void drawSnakes(RenderWindow* window);
